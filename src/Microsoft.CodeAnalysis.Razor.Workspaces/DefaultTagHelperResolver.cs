7b572b642bdb112976b868986ffb962d842816a6
N. Taylor Mullen <nimullen@microsoft.com>
2017-03-20 10:36:28 -0700

    Remove ViewComponent descriptor resolution from tooling.
    #1074
    
    # Conflicts:
    #	src/Microsoft.CodeAnalysis.Razor.Workspaces/DefaultTagHelperResolver.cs

--- src/Microsoft.CodeAnalysis.Razor.Workspaces/DefaultTagHelperResolver.cs
+++ src/Microsoft.CodeAnalysis.Razor.Workspaces/DefaultTagHelperResolver.cs
@@ -24,14 +24,7 @@ namespace Microsoft.CodeAnalysis.Razor
             var errors = new ErrorSink();
 
             VisitTagHelpers(compilation, assemblyNameFilters, descriptors, errors);
-            VisitViewComponents(compilation, assemblyNameFilters, descriptors, errors);
-
             var diagnostics = new List<RazorDiagnostic>();
-            for (var i = 0; i < errors.Errors.Count; i++)
-            {
-                var diagnostic = RazorDiagnostic.Create(errors.Errors[i]);
-                diagnostics.Add(diagnostic);
-            }
             var resolutionResult = new TagHelperResolutionResult(descriptors, diagnostics);
 
             return resolutionResult;
@@ -52,33 +45,6 @@ namespace Microsoft.CodeAnalysis.Razor
                 {
                     var descriptors = factory.CreateDescriptors(type, errors);
                     results.AddRange(descriptors);
-                }
-            }
-        }
-
-        private void VisitViewComponents(Compilation compilation, IEnumerable<string> assemblyNameFilters, List<TagHelperDescriptor> results, ErrorSink errors)
-        {
-            var types = new List<INamedTypeSymbol>();
-            var visitor = ViewComponentTypeVisitor.Create(compilation, types);
-
-            VisitCompilation(visitor, compilation);
-
-            var factory = new ViewComponentTagHelperDescriptorFactory(compilation);
-
-            foreach (var type in types)
-            {
-                try
-                {
-                    if (assemblyNameFilters.Contains(type.ContainingAssembly.Identity.Name))
-                    {
-                        var descriptor = factory.CreateDescriptor(type);
-
-                        results.Add(descriptor);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    errors.OnError(SourceLocation.Zero, ex.Message, length: 0);
                 }
             }
         }
